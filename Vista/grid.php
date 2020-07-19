6acf9e41ed0daf2d3fd803422f6f418647ac9e0d
Igor Llancapan <55776451+igONELL@users.noreply.github.com>
2020-07-19 06:36:03 -0400

    +Grid +Id-Nombre-Precio-Cantidad

--- Vista/grid.php
+++ Vista/grid.php
@@ -11,14 +11,25 @@
   <div class="row">
     <div class="col-sm">
     <img src="Img/seoul3.jpg" alt="..." class="img-thumbnail">
-      One of three columns
+      <p>id</p>
+      <p>Nombre</p>
+      <p>Precio</p>
+      <p>Cantidad</p>
+      <input type="button" name "" value="boton">
     </div>
     <div class="col-sm">
-    <img src="Img/5.png" alt="..." class="img-thumbnail">s
+    <img src="Img/5.png" alt="..." class="img-thumbnail">
+      <p>id</p>
+      <p>Nombre</p>
+      <p>Precio</p>
+      <p>Cantidad</p>
     </div>
     <div class="col-sm">
     <img src="Img/tumblr_mswsnxpGEN1s4yvx4o1_1280.jpg" alt="..." class="img-thumbnail">
-      One of three columns
+      <p>id</p>
+      <p>Nombre</p>
+      <p>Precio</p>
+      <p>Cantidad</p>
     </div>
   </div>
 </div>
